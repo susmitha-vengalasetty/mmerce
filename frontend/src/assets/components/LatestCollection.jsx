c465ed3cf3464220f0deb33de0c67f1f5e263b65
Susmitha Vengalasetty <vengalasettisusmitha@gmail.com>
2026-02-21 18:42:38 +0530

    message

--- frontend/src/assets/components/LatestCollection.jsx
+++ frontend/src/assets/components/LatestCollection.jsx
@@ -10,7 +10,7 @@ const LatestCollection = () => {
     const [latestProducts,setLatestProducts] = useState([]);
 
     useEffect(()=>{
-        setLatestProducts(products.slice(0,10));
+  setLatestProducts(products.slice(0,10));
     },[products])
   return (
     <div className='my-40'>
